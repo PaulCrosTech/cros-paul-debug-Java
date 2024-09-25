1127f636e85a089c6d66f029467a538e69fe0b49
Paul CROS <paul.cros.tech@gmail.com>
2024-09-25 11:49:44 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,13 +3,23 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import ej.blocs.Type;
 
 public class AnalyticsCounter {
+
+	private ISymptomReader ISymptomReader;
+	private ISymptomWriter ISymptomWriter;
+	
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
+		
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
@@ -38,4 +48,37 @@ public class AnalyticsCounter {
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
+
+	
+	public AnalyticsCounter(ISymptomReader ISymptomReader, ISymptomWriter ISymptomWriter)
+	{
+		this.ISymptomReader = ISymptomReader;
+		this.ISymptomWriter = ISymptomWriter;
+	}
+	
+	public List<String> getSymptoms()
+	{
+		return ISymptomReader.GetSymptoms();
+	}
+	
+	public void writeSymptoms(Map<String, Integer> symptoms)
+	{
+		ISymptomWriter.writeSymptoms(symptoms);
+	}
+	
+	public Map<String, Integer> countSymptoms(List<String> symptoms)
+	{
+		Map<String, Integer> quantiteSymptom = new TreeMap<String, Integer>();
+		
+		symptoms.forEach((symptom) -> {
+			quantiteSymptom.merge(symptom, 1, Integer::sum);
+		});
+		
+		return quantiteSymptom;
+	}
+	
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		return symptoms;
+	}
+	
 }
