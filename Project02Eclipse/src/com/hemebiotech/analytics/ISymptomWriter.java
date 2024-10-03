d9e0849212147df81f7b6d25616aa2e6e7055d2a
Paul CROS <paul.cros.tech@gmail.com>
2024-10-03 10:17:16 +0200

    Cleaning code (camelCase, comments)

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -3,11 +3,12 @@ package com.hemebiotech.analytics;
 import java.util.Map;
 
 /**
- * Interface describe how to Write symptoms into a file. 
+ * Interface describe how to Write symptoms into a file.
  */
 public interface ISymptomWriter {
 	/**
-	 * Write the pair symptom / occurrences into a file. 
+	 * Write the pair symptom / occurrences into a file.
+	 * 
 	 * @param symptoms Object Map of symptom / occurrence.
 	 */
 	public void writeSymptoms(Map<String, Integer> symptoms);
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -7,33 +7,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Generate a list of symptoms by reading a file.
- * This class implements ISymptomReader.
+ * Generate a list of symptoms by reading a file. This class implements
+ * ISymptomReader.
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	/**
 	 * Read the symptom file.
+	 * 
 	 * @return Symptom list.
 	 */
 	@Override
-	public List<String> GetSymptoms() {
+	public List<String> getSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
